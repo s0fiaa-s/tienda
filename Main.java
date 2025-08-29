79a9ff31b5013bdb1ab835b48fc6d937134ac644
Sofia <snsandoval@scl.edu.gt>
2025-08-29 07:39:56 -0600

    Actualización Main por corrección

--- Main.java
+++ Main.java
@@ -5,29 +5,21 @@ import views.Dashboard;
 public class Main {
 public static void main(String[] args) {
 
-    String saludo = "Hola Mundo";
-    System.out.println(saludo.charAt(0));
-
-    for (int i = 0; i < saludo.length(); i++) {
-        System.out.println(saludo.charAt(i));
-    }
  
     try {
         Dashboard dashboard = new Dashboard();
 
         //consulta
-        String urlApi = "https://jsonplaceholder.typicode.com/posts";
+        String urlApi = "https://jsonplaceholder.typicode.com/posts/99";
         String respuesta = TiendaFecher.obtenerProductos(urlApi);
         System.out.println("Respuest de la Api");
 
         //Dashboard dashboard = new Dashboard(respuesta);
 
         for (int i = 0; i < respuesta.length(); i++) {
-            String letra ="" + respuesta.charAt(i);
-
-            if (letra.equalsIgnoreCase("}")) {
-                System.out.println(respuesta.charAt(i));
-            }
+          
+                System.out.print(respuesta.charAt(i));
+            
         }
 
 
